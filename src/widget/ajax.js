d6d25c3b8bfabfa689c5322f667254729328bfd3
weiwei <weiwei@xiaomi.com>
2014-10-31 16:40:55 +0800

    add ajax

---
+++ src/widget/ajax.js
@@ -0,0 +1,51 @@
+define([
+    "./core",
+    "jquery"
+], function(Core, $) {
+    var get = function(url, success, error, timeout) {
+        timeout = timeout || 5000;
+        $.ajax({
+            type: "GET",
+            url : url,
+            timeout: timeout,
+            dataType:"json",
+            success:function(data) {
+                if (typeof success == 'function') {
+                    success(data);
+                }
+            },
+            error:function() {
+                if (typeof error == 'function') {
+                    error();
+                }
+            }
+        });
+    }
+    var post = function(url, data, success, error, timeout) {
+        timeout = timeout || 5000;
+        $.ajax({
+            type: "POST",
+            url : url,
+            timeout: timeout,
+            dataType:"json",
+            data : data,
+            success:function(data) {
+                if (typeof success == 'function') {
+                    success(data);
+                }
+            },
+            error:function() {
+                if (typeof error == 'function') {
+                    error();
+                }
+            }
+        });
+    }
+    Core.extend({
+        ajax : {
+            get : get,
+            post : post
+        }
+    });
+    return Core;
+});
