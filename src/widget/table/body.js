621229f1a453c9d2bb7642064627ae01f6fa43a2
weiwei <weiwei@xiaomi.com>
2014-10-31 14:11:17 +0800

    table body

--- src/widget/table/body.js
+++ src/widget/table/body.js
@@ -11,48 +11,49 @@ define([
             me.field = [];
             me.data = [];
             me.groupData = [];
-            me.groupMap = [];
-            me.sum = {};
+            me.sum = {row:[]};
+            me.isGroup = option.dataGroup || false;
+            me.isSum = false;
 
-            var isGroup = option.dataGroup || false;
-            var isSum = false;
             for (var i in option.field) {
                 me.field.push({
-                    numSwitch : option.field[i].numSwitch || false
+                    numSwitch : option.field[i].numSwitch || false,
+                    format : option.field[i].format || false,
+                    formula : option.field[i].formula || false,
+                    dataIndex : option.field[i].dataIndex
                 });
                 if (option.field.isSum || false) {
-                    isSum = true;
+                    me.isSum = true;
                 }
             }
             var tmpMap = [];
             var counter = 0;
             for (var i in option.data) {
-                me.data.push(me.formatRow(option.field, option.data[i]));
-                if (isGroup || isSum) {
-                    if (isGroup) {
+                me.data.push({row: me.formatRow(option.data[i])});
+                if (me.isGroup || me.isSum) {
+                    if (me.isGroup) {
                         var gField = option.field[option.dataGroup.fieldIndex].name;
                         var gValue = option.data[i][option.dataGroup.dataIndex];
                         var idx = tmpMap.indexOf(gValue);
                         if (idx < 0) {
                             tmpMap.push(gValue);
                             idx = gValue.length-1;
-                            me.groupData[idx] = {};
-                            me.groupMap[idx] = [];
-                            me.groupData[idx][gField] = gValue;
+                            me.groupData[idx] = {data: {}, sub : []};
+                            me.groupData[idx].data[gField] = gValue;
+                            me.groupData[idx].sub.push(counter);
                         }
-                        me.groupMap[idx].push(counter);
                     }
                     for (var j in option.data[i]) {
                         if (!Dui.isNumber(option.data[i][j])) {
                             continue;
                         }
-                        if (isSum) {
-                            me.sum[j] = me.sum[j] || 0;
-                            me.sum[j] += option.data[i][j];
+                        if (me.isSum) {
+                            me.sum.data[j] = me.sum.sum[j] || 0;
+                            me.sum.data[j] += option.data[i][j];
                         }
-                        if (isGroup) {
-                            me.groupData[idx][j] = me.groupData[idx][j] || 0;
-                            me.groupData[idx][j] += option.data[i][j];
+                        if (me.isGroup) {
+                            me.groupData[idx].data[j] = me.groupData[idx].data[j] || 0;
+                            me.groupData[idx].data[j] += option.data[i][j];
                         }
                     }
                 }
@@ -60,26 +61,49 @@ define([
             }
         },
         render : function() {
+            var me = this;
+            var tbody = new Dui.dom("tbody");
+            if (me.isGroup) {
+                for (var i in me.groupData) {
+                    var tr = new Dui.dom("tr");
+                    var row = me.formatRow(me.groupData[i].data);
+                    me.groupData[i].row = row;
+                    for (var i in row) {
+                        var td = new Dui.dom("td");
+                        td.text(row[i].format);
+                        tr.append(td);
+                    }
+                    me.groupData[i].dom = tr;
+                    tbody.append(tr);
+                }
+            } else {
+                for (var i in me.data) {
+                    var tr = new Dui.dom("tr");
+                    for (var j in me.data[i].row) {
+                        var td = new Dui.dom("td");
+                        td.text(me.data[i].row[j].format);
+                        tr.append(td);
+                    }
+                    me.data[i].dom = tr;
+                    tbody.append(tr);
+                }
+            }
+            me.box.append(tbody);
         },
-        formatRow : function(field, data) {
+        formatRow : function(data) {
             var row = [];
-            for (var i in field) {
+            for (var i in me.field) {
                 var num = Dui.var.defaultVar;
-                if (typeof field[i].formula === "function") {
-                    num = field[i].formula(data);
-                } else if (typeof data[field[i].dataIndex] !== "undefined") {
-                    num = data[field[i].dataIndex];
+                if (typeof me.field[i].formula === "function") {
+                    num = me.field[i].formula(data);
+                } else if (typeof data[me.field[i].dataIndex] !== "undefined") {
+                    num = data[me.field[i].dataIndex];
                 }
                 var format = num;
-                if (typeof field[i].format === "function") {
-                    format = field[i].format(num);
+                if (typeof me.field[i].format === "function") {
+                    format = me.field[i].format(num);
                 }
-
-                var cell = {show: format};
-                if (field.numSwitch || false) {
-                    cell.num = num;
-                }
-                row.push(cell);
+                row.push({format: format, num: num});
             });
             return row;
         },
